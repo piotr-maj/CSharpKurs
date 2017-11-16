29d54a3991504962b2b5c73714cd4fcca8d5ac7a
Piotr Maj <piotr.maj@outlook.com>
2017-11-16 14:13:47 +0100

    wszystko z odcinka 7 KLASY

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+
+        // stan klasy
+        // ratings - zmienna oceny
+
+        List<float> ratings;
+
+        // achowania klasy, metody
+        // rating - zemianna ocena
+
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0;
+
+            foreach (var rating in ratings)
+            {
+                sum = sum + rating;
+                // zapis równoznaczny sum +=rating;
+                                            }
+            avg = sum / ratings.Count();
+            //Count - daje ilość elementów w kolekcji
+
+            return avg;
+        }
+        
+        public float GiveMaxRating()
+        {
+
+            return ratings.Max();
+        }
+
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+
+
+    }
+}
