cf7902102911785363e379ee0ea5bdb7200f038d
Piotr Maj <piotr.maj@outlook.com>
2017-11-24 14:41:27 +0100

    piąteczek 24.11.2017

--- StudentDiary/Program.cs
+++ StudentDiary/Program.cs
@@ -13,7 +13,18 @@ namespace StudentDiary
 
             Diary diary = new Diary();
             diary.AddRating(5);
+            diary.AddRating(3.5f);
+            diary.AddRating(1.5f);
 
+            float avg = diary.CalculateAverage();
+            float min = diary.GiveMinRating();
+            float max = diary.GiveMaxRating();
+
+            for ( ; ; )
+            {
+                Console.WriteLine("Podaj ocenę w zakresie 1 - 10");
+                float rating = Console.ReadLine();
+            }
         }
     }
 }
